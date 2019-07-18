f258f080b8540f1cd0de660128bd200230171e0d
Sebastian Friedemann <s.friedemann@gmx.net>
2019-07-18 09:34:10 +0200

    Always recompile

--- api/compile.sh
+++ api/compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+rm test.o
 g++ -std=c++11 -c -fpermissive -fPIC  melissa_api.cxx -o test.o -lmpi -lzmq -I../../melissa/install/include -L../../melissa/install/lib -I/usr/include/mpi -L/usr/lib
