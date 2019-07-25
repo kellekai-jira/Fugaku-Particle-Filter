4a791a204bf379331d29a1c0f576f87b75b6099b
Sebastian Friedemann <s.friedemann@gmx.net>
2019-07-25 10:02:01 +0200

    Catch Ctrl+C in run script to close gdb

--- example-simulation/run.sh
+++ example-simulation/run.sh
@@ -2,11 +2,29 @@
 
 killall xterm
 
+precommand="xterm_gdb"
 
-for i in `seq 0 3`;
+if [[ "$1" == "test" ]];
+then
+  precommand=""
+fi
+
+
+
+for i in `seq 0 1`;
 do
   echo start simu id $i
-  mpirun -n 2 -x MELISSA_SIMU_ID=$i -x MELISSA_SERVER_MASTER_NODE="tcp://narrenkappe:4000" -x LD_LIBRARY_PATH=/home/friese/workspace/melissa-da/build_api:/home/friese/workspace/melissa/install/lib xterm_gdb /home/friese/workspace/melissa-da/build_example-simulation/example_simulation &
+  mpirun -n 2 -x MELISSA_SIMU_ID=$i -x MELISSA_SERVER_MASTER_NODE="tcp://narrenkappe:4000" -x LD_LIBRARY_PATH=/home/friese/workspace/melissa-da/build_api:/home/friese/workspace/melissa/install/lib $precommand /home/friese/workspace/melissa-da/build_example-simulation/example_simulation &
+
 done
+
+# trap ctrl-c and call ctrl_c()
+trap ctrl_c INT
+
+function ctrl_c() {
+        echo "** Trapped CTRL-C"
+        killall xterm
+        exit 0
+}
 
 wait
