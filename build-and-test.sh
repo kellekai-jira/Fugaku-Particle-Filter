9aaa3c0ceac6928198e883a4ce282ec3df89ae9f
Christoph Conrads <christoph.conrads@inria.fr>
2020-11-18 17:45:36 +0000

    CI: increase time-out

--- build-and-test.sh
+++ build-and-test.sh
@@ -108,4 +108,4 @@ cmake \
 	-- "$melissa_da_source_dir"
 cmake --build . -- --jobs="$num_jobs"
 cmake --build . --target install
-ctest --output-on-failure --timeout 60
+ctest --output-on-failure --timeout 300
