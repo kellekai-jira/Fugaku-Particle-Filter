355c71675894208b8a1ccb227c33784cca6ae107
sfriedem <sebastian.friedemann@inria.de>
2020-12-01 14:52:25 +0100

    Studies with deadlocking simulations shall crash, so clean up zombie processes manually

--- test/test-unresponsive-runner.py
+++ test/test-unresponsive-runner.py
@@ -1,18 +1,23 @@
 from melissa_da_testing import *
 
+
+local_cluster = LocalCluster()
+EXEC = 'simulation1-deadlock'
 run_melissa_da_study(
-    runner_cmd='simulation1-deadlock',
+    runner_cmd=EXEC,
     runner_timeout=5,  # detect tests very fast. Still this may not be too tight as the launcher uses the same timeout to detect if a runner started up. so it must be larger than the runners startup time.
     total_steps=3,
     ensemble_size=10,
     assimilator_type=ASSIMILATOR_DUMMY,
-    cluster=LocalCluster(),
+    cluster=local_cluster,
     procs_server=3,
     procs_runner=2,
     n_runners=2,
     show_server_log=False,
     show_simulation_log=False)
 
+# Since the study will crash we manually clean up:
+local_cluster.CleanUp(EXEC)
 
 found1 = False
 found2 = False
