abd095f84a211e5f2c2ef2a346b475858ea24ad3
Christoph Conrads <christoph.conrads@inria.fr>
2021-03-10 16:08:22 +0000

    Prevent unaligned memory accesses in C, C++

--- common/melissa_da_stype.h
+++ common/melissa_da_stype.h
@@ -4,16 +4,20 @@
 #define VEC_T char
 #define VARID_T int  // TODO: change to short in future? As I do not know for now how to do shorts in Fortran I stay with ints so far.
 
-//#define INDEX_MAP_T unsigned int  // TODO:  use this as index map type?!
-//#define MPI_INDEX_MAP_T MPI_UNSIGNED //_INT
+static_assert(alignof(VEC_T) == 1,
+              "VEC_T alignment must be 1 to avoid problems with unaligned memory accesses");
 
-typedef struct index_map_s {
+// #define INDEX_MAP_T unsigned int  // TODO:  use this as index map type?!
+// #define MPI_INDEX_MAP_T MPI_UNSIGNED //_INT
+
+typedef struct index_map_s
+{
     int index;
     int varid;
 } index_map_t;
 
 #define INDEX_MAP_T index_map_t
-//#define MPI_INDEX_MAP_T MPI_INT
+// #define MPI_INDEX_MAP_T MPI_INT
 
 
 
