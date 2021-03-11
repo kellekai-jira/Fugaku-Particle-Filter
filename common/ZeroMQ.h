266f19b2970701ced7ac2bb5e560dad8f0cd0d56
Christoph Conrads <christoph.conrads@inria.fr>
2021-03-11 14:36:53 +0000

    ZeroMQ: clarify resource ownership zmq::msg_init
    Clarify the resource ownership and requirements by the `zmq::msg_init()`
    overload with four arguments.

--- common/ZeroMQ.h
+++ common/ZeroMQ.h
@@ -22,6 +22,23 @@ using FreeFn = void (*)(void*, void*);
 char* data(Message& msg);
 zmq::MessageRef msg_init();
 zmq::MessageRef msg_init(std::size_t size);
+/**
+ * This function initializes a new 0MQ message with the data given by the
+ * first argument.
+ *
+ * If free is not NULL, then 0MQ assumes ownership over the data referenced by
+ * the first argument and as soon as the message was sent,
+ * it will free the resources occupied the data by calling free(data, hints).
+ *
+ * @attention This function does *not* copy the data. Instead, the storage
+ * referenced by the first argument must not be modified until the message was
+ * sent.
+ *
+ * @param[in] data Contents to be sent
+ * @param[in] size Message size in bytes
+ * @param[in] free Reference to a function freeing resources referenced by data
+ * @param[in] hints Value to be passed verbatim to the function free
+ */
 zmq::MessageRef msg_init(
     void* data, std::size_t size, FreeFn free = nullptr, void* hints = nullptr);
 
