c9eca947a9241a6f52ac5dd8b2b8a447095b58da
Christoph Conrads <christoph.conrads@inria.fr>
2021-03-15 17:25:31 +0000

    Test: fix typos

--- test/test-never-connecting-runner.py
+++ test/test-never-connecting-runner.py
@@ -12,7 +12,8 @@ ae = {
 }
 
 
-def run_study_and_wait_for(on_event, study_options):  #TODO: use this function for all othe rtestcases in this folder too
+# TODO: use this function for all other testcases in this folder, too
+def run_study_and_wait_for(on_event, study_options):
     tmpdir = tempfile.mkdtemp()
     fifo_name_server = os.path.join(tmpdir, 'server_fifo')
     runners = 0
@@ -25,7 +26,7 @@ def run_study_and_wait_for(on_event, study_options):  #TODO: use this function f
 
     os.mkfifo(fifo_name_server)
 
-    if not 'additioinal_server_env' in study_options:
+    if not 'additional_server_env' in study_options:
         study_options['additional_server_env'] = {}
     study_options['additional_server_env']['MELISSA_DA_TEST_FIFO'] = fifo_name_server
     study = Process(target=run)
