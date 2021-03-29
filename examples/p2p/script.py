10702dc24c218bc82d612bdd43475905649ba12b
sfriedem <sebastian.friedemann@inria.de>
2021-03-29 16:19:33 +0200

    Autogenerate config.fti

--- examples/p2p/script.py
+++ examples/p2p/script.py
@@ -1,15 +1,29 @@
 import os
 import shutil
+import configparser
 
 os.system('killall gdb')
 
 from melissa_da_study import *
 clean_old_stats()
 
+PROCS_RUNNER = 3
+NODES_RUNNER = 1
+
+assert PROCS_RUNNER >= 2
+assert NODES_RUNNER >= 1
+assert PROCS_RUNNER % NODES_RUNNER == 0
+
 def prepare_runner_dir():
     print('Preparing runner directory (%s)' % os.getcwd())
     shutil.copy('../../config-runner.fti', './config.fti')
-    pass
+
+    config = configparser.ConfigParser()
+    config.read('config.fti')
+    config['basic']['node_size'] = str(PROCS_RUNNER//NODES_RUNNER)
+    with open('config.fti', 'w') as f:
+        config.write(f)
+
 
 run_melissa_da_study(
     server_cmd='xterm_gdb python3.6m -u %s/server-p2p/server.py' % os.getenv('MELISSA_DA_SOURCE_PATH'),  # Activate this line to start the weight server instead!
@@ -17,9 +31,8 @@ run_melissa_da_study(
     runner_cmd='xterm_gdb simulation4-p2p',
     total_steps=10,
     ensemble_size=30,
-    procs_server=1,
-    procs_runner=2,
-    n_runners=1,
+    procs_runner=PROCS_RUNNER,
+    n_runners=NODES_RUNNER,
     create_runner_dir=True,
     prepare_runner_dir=prepare_runner_dir,
     show_server_log=False,
