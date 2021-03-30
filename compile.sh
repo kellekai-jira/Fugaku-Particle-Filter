f4e011044e686be020ea7ebba9f95e1c28ca3e45
Sebastian Friedemann <sebastian.friedemann@inria.fr>
2021-03-30 16:39:26 +0200

    Use precompiled fti on juwels

--- compile.sh
+++ compile.sh
@@ -78,7 +78,7 @@ else
           -DWITH_FTI=ON -DCMAKE_BUILD_TYPE=Debug \
           -DWITH_FTI_THREADS=ON \
           -DREPORT_TIMING_ALL_RANKS=ON \
-          -DSLOW_MPI_DIR="/p/scratch/prcoe03/tmp"
+          -DSLOW_MPI_DIR="/p/scratch/prcoe03/tmp" \
           -DFTI_PATH=/p/project/prcoe03/sebastian/wrf2/FTI-p2p
 
     else
