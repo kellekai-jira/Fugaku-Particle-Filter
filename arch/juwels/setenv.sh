3ee74ff72d473446071e96c838beb33d5a30faa8
sfriedem <sebastian.friedemann@inria.de>
2021-03-31 14:49:05 +0200

    Update juwels setenv

--- arch/juwels/setenv.sh
+++ arch/juwels/setenv.sh
@@ -11,5 +11,10 @@ module load HDF5
 module load GDB/10.1
 module load CMake/3.18.0
 
+# Python Assimilator
+module load Python/3.8.5
+module load mpi4py/3.0.3-Python-3.8.5
+module load netcdf4-python/1.5.4-Python3.8.5
+
 ulimit -s unlimited
 ulimit -c 4000000
