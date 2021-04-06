17c52edd450c92767013df66bc399f57026c42f9
kellekai <kai.keller@bsc.es>
2021-04-06 09:35:26 +0200

    corrected compile.sh

--- compile.sh
+++ compile.sh
@@ -3,32 +3,67 @@
 #./uncrustify.sh
 mkdir -p build
 cd build
-#if [ "$ARCH" == "marenostrum" ];
-#then
-#    echo "INSTALL ON MARENOSTRUM..."
-#    CC=gcc CXX=g++ FC=gfortran cmake .. -DWITH_FTI=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=install \
-#     -DWITH_FTI_THREADS=ON -DFTI_PATH=/gpfs/projects/bsc93/bsc93655/melissaP2P/FTI/fti/install \
-#     -DREPORT_TIMING_ALL_RANKS=ON -DPDAF_PATH=$PROJECTS/melissaP2P/Melissa/PDAF \
-#     -DProtobuf_INCLUDE_DIR=/apps/PROTOBUF/3.5.1/INTEL/include -DProtobuf_LIBRARY=/apps/PROTOBUF/3.5.1/INTEL/lib/libprotobuf.so \
-#    # -DCMAKE_C_FLAGS=-D_POSIX_C_SOURCE=199309L \
-#    # -DCMAKE_CXX_FLAGS="-D_GLIBCXX_USE_CXX11_ABI=0 -std=c++14"
-#    #cmake .. -DWITH_FTI=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=install -DHDF5_ROOT=/usr/lib/x86_64-linux-gnu/hdf5/openmpi \
-#    # -DWITH_FTI_THREADS=ON -DFTI_PATH=/gpfs/projects/bsc93/bsc93655/melissaP2P/FTI/fti/install \
-#    # -DZeroMQ_INCLUDE_DIR=$PROJECTS/melissaFT/opt/ZeroMQ/zeromq-marenostrum/install-4.3.4-marenostrum/include \
-#    # -DZeroMQ_LIBRARY=$PROJECTS/melissaFT/opt/ZeroMQ/zeromq-marenostrum/install-4.3.4-marenostrum/lib64/libzmq.so \
-#    # -DREPORT_TIMING_ALL_RANKS=ON -DPDAF_PATH=$PROJECTS/melissaP2P/Melissa/PDAF -DZeroMQ_VERSION="4.3.4" \
-#    # -DProtobuf_INCLUDE_DIR=/apps/PROTOBUF/3.5.1/INTEL/include -DProtobuf_LIBRARY=/apps/PROTOBUF/3.5.1/INTEL/lib/libprotobuf.so \
-#    # -DCMAKE_C_FLAGS=-D_POSIX_C_SOURCE=199309L \
-#    # -DCMAKE_CXX_FLAGS="-D_GLIBCXX_USE_CXX11_ABI=0 -std=c++14" \
-#
-#
-if [ "$(hostname)" == "narrenkappe" ];
+if [ "$ARCH" == "marenostrum" ];
+then
+    echo "INSTALL ON MARENOSTRUM..."
+    CC=mpicc CXX=mpic++ FC=mpifort cmake .. \
+      -DCMAKE_BUILD_TYPE=Debug \
+      -DCMAKE_INSTALL_PREFIX=install \
+      -DCMAKE_CXX_COMPILER=mpicxx \
+      -DCMAKE_Fortran_COMPILER=mpif90 \
+      -DCMAKE_C_COMPILER=mpicc \
+      -DINSTALL_FTI=OFF \
+      -DWITH_FTI=ON \
+      -DWITH_FTI_THREADS=ON \
+      -DREPORT_TIMING_ALL_RANKS=ON \
+      -DFTI_PATH=/gpfs/projects/bsc93/bsc93655/melissaP2P/FTI/fti/install \
+      -DCMAKE_PREFIX_PATH=melissa/install/share/cmake/ZeroMQ \
+      -DProtobuf_LIBRARIES=/apps/PROTOBUF/3.5.1/INTEL/lib/libprotobuf.so \
+      -DProtobuf_INCLUDE_DIR=/apps/PROTOBUF/3.5.1/INTEL/include \
+      -DCMAKE_CXX_FLAGS="-D_GLIBCXX_USE_CXX11_ABI=0 -std=c++14"
+
+
+elif [ "$(hostname)" == "narrenkappe" ];
 then
     #cmake .. -DZeroMQ_DIR=$HOME/workspace/melissa/install/share/cmake/ZeroMQ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=install
 if [ "$MELISSA_PROFILING" == "" ];
 then
-    cmake .. -DINSTALL_FTI=ON -DWITH_FTI=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=install -DHDF5_ROOT=/usr/lib/x86_64-linux-gnu/hdf5/openmpi \
+    cmake .. -DINSTALL_FTI=OFF -DWITH_FTI=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=install -DHDF5_ROOT=/usr/lib/x86_64-linux-gnu/hdf5/openmpi \
      -DWITH_FTI_THREADS=ON \
+     -DREPORT_TIMING_ALL_RANKS=ON \
+    -DFTI_PATH=$HOME/tmp/FTI
+    #cmake .. -DWITH_FTI=OFF -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=install \
+     #-DREPORT_TIMING_ALL_RANKS=ON
+else
+    F90="wrapper-f90.sh"
+    CC="wrapper-cc.sh"
+    CXX="wrapper-cxx.sh"
+
+    #F90=scorep-gfortran
+    #CC=scorep-gcc
+    #CXX=scorep-g++
+    #make SCOREP_WRAPPER_INSTRUMENTER_FLAGS="--user" SCOREP_WRAPPER_COMPILER_FLAGS="-g –O2"
+    cd ..
+    export PATH="$PWD/profiling:$PATH"
+    cd build
+    cmake .. -DINSTALL_FTI=OFF -DWITH_FTI=OFF -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=install -DHDF5_ROOT=/usr/lib/x86_64-linux-gnu/hdf5/openmpi \
+     -DWITH_FTI_THREADS=ON -DCMAKE_CXX_COMPILER="$CXX" -DCMAKE_C_COMPILER="$CC" -DCMAKE_Fortran_COMPILER="$F90"
+fi
+    #cmake .. -DWITH_FTI=OFF -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=install -DHDF5_ROOT=/usr/lib/x86_64-linux-gnu/hdf5/openmpi
+elif [ "$USER" == "rkop006" ];
+then
+	# jean zay:
+        cmake .. \
+          -DCMAKE_BUILD_TYPE=Debug \
+          -DCMAKE_INSTALL_PREFIX=install
+elif [ "$(hostname)" == "linux-rol5" ];
+then
+    #cmake .. -DZeroMQ_DIR=$HOME/workspace/melissa/install/share/cmake/ZeroMQ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=install -DMPIEX....
+if [ "$MELISSA_PROFILING" == "" ];
+then
+    cmake .. -DINSTALL_FTI=OFF -DWITH_FTI=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=install -DHDF5_ROOT=/home/$USER/opt/HDF5/GCC/1.10.5 \
+     -DWITH_FTI_THREADS=ON \
+     -DFTI_PATH=/home/kellekai/STUDY/PhD/Research/Melissa-P2P-Lab/FTI/fti-gitlab/install \
      -DREPORT_TIMING_ALL_RANKS=ON
     #cmake .. -DWITH_FTI=OFF -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=install \
      #-DREPORT_TIMING_ALL_RANKS=ON
@@ -44,36 +79,27 @@ else
     cd ..
     export PATH="$PWD/profiling:$PATH"
     cd build
-    cmake .. -DINSTALL_FTI=ON -DWITH_FTI=OFF -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=install -DHDF5_ROOT=/usr/lib/x86_64-linux-gnu/hdf5/openmpi \
+    cmake .. -DINSTALL_FTI=ON -DWITH_FTI=OFF -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=install -DHDF5_ROOT=/home/kellekai/opt/HDF5/GCC/1.10.5 \
      -DWITH_FTI_THREADS=ON -DCMAKE_CXX_COMPILER="$CXX" -DCMAKE_C_COMPILER="$CC" -DCMAKE_Fortran_COMPILER="$F90"
 fi
-    #cmake .. -DWITH_FTI=OFF -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=install -DHDF5_ROOT=/usr/lib/x86_64-linux-gnu/hdf5/openmpi
-elif [ "$USER" == "rkop006" ];
-then
-	# jean zay:
-        cmake .. \
-          -DCMAKE_BUILD_TYPE=Debug \
-          -DCMAKE_INSTALL_PREFIX=install
 else
     # juwels...
     if [ "$MELISSA_PROFILING" == "" ];
     then
         echo here, juwels...
-        CC=mpicc CXX=mpic++ FC=mpifort cmake .. \
+        cmake .. \
           -DCMAKE_BUILD_TYPE=Debug \
           -DCMAKE_INSTALL_PREFIX=install \
           -DCMAKE_CXX_COMPILER=mpicxx \
           -DCMAKE_Fortran_COMPILER=mpif90 \
           -DCMAKE_C_COMPILER=mpicc \
+          -DZeroMQ_ROOT=$HOME/workspace/melissa-da/melissa/install \
           -DINSTALL_FTI=OFF \
-          -DWITH_FTI=ON \
+          -DWITH_FTI=ON -DCMAKE_BUILD_TYPE=Debug \
           -DWITH_FTI_THREADS=ON \
           -DREPORT_TIMING_ALL_RANKS=ON \
-          -DFTI_PATH=/gpfs/projects/bsc93/bsc93655/melissaP2P/FTI/fti/install \
-          -DCMAKE_PREFIX_PATH=melissa/install/share/cmake/ZeroMQ \
-          -DProtobuf_LIBRARIES=/apps/PROTOBUF/3.5.1/INTEL/lib/libprotobuf.so \
-          -DProtobuf_INCLUDE_DIR=/apps/PROTOBUF/3.5.1/INTEL/include \
-          -DCMAKE_CXX_FLAGS="-D_GLIBCXX_USE_CXX11_ABI=0 -std=c++14"
+          -DSLOW_MPI_DIR="/p/scratch/prcoe03/tmp" \
+          -DFTI_PATH=/p/project/prcoe03/sebastian/wrf2/FTI-p2p
 
     else
         echo here, juwels with profiling...
