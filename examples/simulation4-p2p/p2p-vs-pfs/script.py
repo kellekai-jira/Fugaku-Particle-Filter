3559b7a569dadbedd63def875f76cb97e0e4b6e7
sfriedem <sebastian.friedemann@inria.fr>
2021-05-06 11:11:12 +0200

    Run on jean-zay to test p2p vs pfs
    pfs wins... For now

--- examples/simulation4-p2p/p2p-vs-pfs/script.py
+++ examples/simulation4-p2p/p2p-vs-pfs/script.py
@@ -12,28 +12,41 @@ from melissa_da_study import *
 
 start_time = time.time()
 
-NODES_SERVER = 1
+IS_P2P = True
+NODES_SERVER = 1 if IS_P2P else 16
 NODES_RUNNER = 1
+MEMBERS = 100
+
+N_RUNNERS = 1
+if IS_P2P:
+    print('Doing a p2p study')
+    sn = os.getenv('SLURM_NNODES')
+    if sn:
+        N_RUNNERS=int(sn) - 1  # subtract server node
+        MEMBERS = N_RUNNERS * 11
+
+        print(f'Doing a study, members={MEMBERS}, runners={N_RUNNERS}')
 
 def run():
     clean_old_stats()
     run_melissa_da_study(
             runner_cmd='/gpfsscratch/rech/moy/rkop006/melissa-p2p/examples/simulation4-p2p/test-elasticity/runner.sh',
             total_steps=100000,  # 10e3 are about 5 minuts be longer for sure ;)
-            ensemble_size=1024,
-            n_runners=49,
+            ensemble_size=MEMBERS,
+            n_runners=N_RUNNERS,
             # ensemble_size=102,
             # n_runners=4,
             assimilator_type=ASSIMILATOR_DUMMY,
             # not necessary to add cluster. By default an automatic selection for the cluster
             # is done. See the cluster_selector() method.
             cluster=SlurmCluster('igf@cpu'),
-            procs_server=1,
+            procs_server=1 if IS_P2P else NODES_SERVER * 40,
             nodes_server=NODES_SERVER,
+            local_ckpt_dir='/dev/shm',
             procs_runner=40,
             nodes_runner=NODES_RUNNER,
-            server_timeout=120,
-            runner_timeout=20,
+            server_timeout=500,
+            runner_timeout=500 if IS_P2P else 240,
             show_server_log=False,
             show_simulation_log=False,
             additional_server_env={  # necessary for jean-zay
@@ -46,10 +59,13 @@ def run():
                 'SIMULATION_RANDOM_PROPAGATION_TIME': '1',
                 'MELISSA_DA_TIMING_REPORT': str(time.time() + 60*16),  # write timing events after 16 mins
                 },
-            is_p2p=True)
+            is_p2p=IS_P2P)
 
 HOME = os.getenv("HOME")
 en = 'p2p-vs-pfs'
+if not IS_P2P:
+    en = 'central-server'
+
 repex.run(
         EXPERIMENT_NAME=en,
         INPUT_FILES=['/gpfsscratch/rech/moy/rkop006/melissa-p2p/build/CMakeCache.txt'],
