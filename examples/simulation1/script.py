f7ca6b1914ec1f1578f05f19dba82166d6b439a3
Kai Keller <kai.keller@bsc.es>
2021-08-20 22:28:03 +0900

    finalized prototype of fugaku cluster class

--- examples/simulation1/script.py
+++ examples/simulation1/script.py
@@ -2,7 +2,8 @@ from melissa_da_study import *
 
 run_melissa_da_study(
         runner_cmd='simulation1',
-        total_steps=10,
+        total_steps=1000,
+        cluster=FugakuCluster(),
         ensemble_size=3,
         assimilator_type=ASSIMILATOR_DUMMY,
         # not necessary to add cluster. By default an automatic selection for the cluster
@@ -10,6 +11,6 @@ run_melissa_da_study(
         #cluster=LocalCluster(),
         procs_server=2,
         procs_runner=3,
-        n_runners=1,
+        n_runners=5,
         show_server_log=False,
         show_simulation_log=False)
