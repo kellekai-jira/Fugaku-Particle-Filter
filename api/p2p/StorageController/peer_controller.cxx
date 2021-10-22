41bd1ddfbebc4cbf33db043df9a68aa43b4337a7
Kai Keller <kai.keller@bsc.es>
2021-10-22 11:58:33 +0200

    increase polling time for answer from peer

--- api/p2p/StorageController/peer_controller.cxx
+++ api/p2p/StorageController/peer_controller.cxx
@@ -298,7 +298,7 @@ bool PeerController::get_state_from_peer(const io_state_id_t & state_id,
     send_message(state_request_socket, state_request);
 
     srand(time(NULL) + runner_id);
-    int poll_ms = 1000 + rand() % 3000;
+    int poll_ms = 10000 + rand() % 3000;
     long long stop_poll_ms = now_ms() + poll_ms;
     MDBG("%s has %d ms to send me state %d,%d", port_name.c_str(), poll_ms, state_id.t, state_id.id);
     while (found == false && now_ms() < stop_poll_ms) {
