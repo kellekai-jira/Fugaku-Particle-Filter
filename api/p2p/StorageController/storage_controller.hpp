ef91fb480129ba9b5e1b65f180287ca7183dc15e
Kai Keller <kai.keller@bsc.es>
2022-03-01 17:26:56 +0100

    start with validation implementation

--- api/p2p/StorageController/storage_controller.hpp
+++ api/p2p/StorageController/storage_controller.hpp
@@ -18,7 +18,7 @@
 
 #define IO_PROBE( tag, func ) do { if( storage.m_io->probe( tag ) ) {func; return;} } while(0)
 
-#define STORAGE_MAX_PREFETCH 20 // 5 states
+#define STORAGE_MAX_PREFETCH 100 // 5 states
 
 using namespace melissa_p2p;
 
