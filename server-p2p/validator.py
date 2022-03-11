299555a3fd100d641d88e4d34bc7ff264c8f439d
Kai Keller <kai.keller@bsc.es>
2022-03-11 15:55:48 +0100

    update

--- server-p2p/validator.py
+++ server-p2p/validator.py
@@ -888,8 +888,8 @@ def validate(meta, compare_function, compare_reduction, evaluate_function,
                 for rank, data in enumerate(stddev[name]):
                     stddev[name][rank] = np.sqrt(data/weight_norm)
                     zeroitems = [ii for ii, ee in enumerate(stddev[name][rank]) if ee == 0]
-                    for i in zeroitems:
-                        print(f"[p:{p.id}] stddev[{name}][{rank}][{i}] is zero for avg: {average[name][rank][i]}")
+                    for kk in zeroitems:
+                        print(f"[p:{p.id}] stddev[{name}][{rank}][{kk}] is zero for avg: {average[name][rank][kk]}")
                     #print(f"ensemble stddev[{p.id},{name},{rank}]: {stddev[name][rank][0:3]}")
             if global_weights[i].state_id in state_ids:
                 sid = encode_state_id(global_weights[i].state_id.t, global_weights[i].state_id.id, p.id)
