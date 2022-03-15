8db64cabef0cc83530ac310bd4531d21cacce799
Kai Keller <kai.keller@bsc.es>
2022-03-15 09:09:30 +0100

    update

--- server-p2p/validator.py
+++ server-p2p/validator.py
@@ -598,6 +598,7 @@ def ensemble_mean(proc, weight, cpc, name, meta, proc_data):
             ssum.append(weight.weight * x)
     else:
         for i, x in enumerate(proc_data[proc]):
+            print(f"process: {x} iter: {i}")
             ssum.append(weight.weight * (x + state_buffer[sid][proc][i]))
 
     return ssum
