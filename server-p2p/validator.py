5af53d8f9024997bf04c38874402d910e407f435
Kai Keller <kai.keller@bsc.es>
2022-03-21 09:09:17 +0100

    start lorenz in chaotic state

--- server-p2p/validator.py
+++ server-p2p/validator.py
@@ -820,8 +820,9 @@ def validate(meta, compare_function, compare_reduction, evaluate_function,
         average_x.append(df_avg['value'][0])
         print(f'|   -> computing range')
         df_range = create_dataframe_row('state1', 'range', xmax - xmin, cpc[0], state_id.t, state_id.id, 1.0, original)
+        range = df_range['value'].iloc[-1]
         print('| ')
-        print(f"|       range: {df_range['value'].iloc[-1]}")
+        print(f"|       range: {range}")
         print('| ')
         df_evaluate = df_evaluate.append( pd.concat( [df_avg, df_vmax, df_vmin, df_range], ignore_index=True ), ignore_index=True )
         for p in cpc[1:]:
@@ -855,17 +856,24 @@ def validate(meta, compare_function, compare_reduction, evaluate_function,
             print(f'|   -> computing RMSE of compressed state')
             trigger(START_COMPUTE_RMSE_VALIDATOR, 0)
             df_rmse = compare_wrapper( variables, [original, compared], ndims, nprocs, meta, sse, reduce_sse, 'RMSE', cpc)
-            trigger(STOP_COMPUTE_RMSE_VALIDATOR, 0)
             rmse = df_rmse['value'].iloc[-1]
+            nrmse = rmse/range
+            df_nrmse = create_dataframe_row('state1', 'nrmse', nrmse, p, state_id.t, state_id.id, cpc_rate, compared)
+            trigger(STOP_COMPUTE_RMSE_VALIDATOR, 0)
             print('| ')
             print(f"|       RMSE: {rmse}")
+            print(f"|       RMSE (normalized): {nrmse}")
             print('| ')
             print(f'|   -> computing pointwise maximum error of compressed')
             trigger(START_COMPUTE_PEMAX_VALIDATOR, 0)
             df_emax = compare_wrapper( variables, [original, compared], ndims, nprocs, meta, pme, reduce_pme, 'PE_max', cpc)
+            emax = df_emax['value'].iloc[-1]
+            nemax = emax/range
+            df_nemax = create_dataframe_row('state1', 'nemax', nemax, p, state_id.t, state_id.id, cpc_rate, compared)
             trigger(STOP_COMPUTE_PEMAX_VALIDATOR, 0)
             print('| ')
-            print(f"|       PE_max: {df_emax['value'].iloc[-1]}")
+            print(f"|       PE_max: {emax}")
+            print(f"|       PE_max (normalized): {nemax}")
             print('| ')
             print(f'|   -> computing peak signal to noise ratio')
             psnr = 20 * np.log10( xmax / rmse )
@@ -873,7 +881,7 @@ def validate(meta, compare_function, compare_reduction, evaluate_function,
             print('| ')
             print(f"|       PSNR: {psnr}")
             print('| ')
-            df_evaluate = df_evaluate.append( pd.concat([df_avg_compared, df_rho, df_rmse, df_emax, df_psnr] , ignore_index=True ), ignore_index=True )
+            df_evaluate = df_evaluate.append( pd.concat([df_avg_compared, df_rho, df_rmse, df_nrmse, df_emax, df_nemax, df_psnr] , ignore_index=True ), ignore_index=True )
 
     weights_temp = weights.copy()
     global_weights = allreduce_weights( validators, weights )
