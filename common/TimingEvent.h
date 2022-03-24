61a21298075af14df41430361229120eb9b90f15
Kai Keller <kai.keller@bsc.es>
2022-03-24 10:12:13 +0100

    update

--- common/TimingEvent.h
+++ common/TimingEvent.h
@@ -191,7 +191,7 @@ private:
     std::unique_ptr<std::ofstream> fifo_os;
 
     time_t report_time = 0ll;
-    int m_report_cycle = -100
+    int m_report_cycle = -100;
 
 public:
     std::list<TimingEvent> events;  // a big vector should be more performant!
